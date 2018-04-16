10af9a12d7e704fa0f306fce18dada9422752924
Thi Pham <uea62238@gmail.com>
2018-04-16 21:01:30 +0700

    complete the interface theme and issues!

--- onlineshopping/src/main/java/com/thipham/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/thipham/onlineshopping/controller/PageController.java
@@ -12,7 +12,23 @@ public class PageController {
 	@RequestMapping(value= {"/","/index","/home"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting","welcome to spring web");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
+		return mv;
+	}
+	
+	@RequestMapping(value= "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickHome",true);
+		return mv;
+	}
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
 	}
 //	@RequestMapping(value="/test")
