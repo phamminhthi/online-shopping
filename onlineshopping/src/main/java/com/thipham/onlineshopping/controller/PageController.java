77f92890711c3d6e3f0455ae96e191e31af38df7
Thi Pham <uea62238@gmail.com>
2018-04-16 19:39:54 +0700

    Successfully Loaded Static Resources

---
+++ onlineshopping/src/main/java/com/thipham/onlineshopping/controller/PageController.java
@@ -0,0 +1,41 @@
+package com.thipham.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/index","/home"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring web");
+		return mv;
+	}
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+//		if(greeting==null) {
+//			greeting ="ahihi do ngoc";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//		
+//		
+//	}
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting") String greeting) {
+		if(greeting==null) {
+			greeting ="ahihi do ngoc";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+		
+		
+	}
+
+}
